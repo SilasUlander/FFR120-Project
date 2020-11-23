ce4380d4cd2e537643d4d11eeaf41c7d5a285350
Silas Ulander <silasu@student.chalmers.se>
2020-11-23 11:17:21 +0100

    Added fig.show

--- Liseberg_Folder/AmusementParkOptimizer.py
+++ Liseberg_Folder/AmusementParkOptimizer.py
@@ -6,59 +6,57 @@ import random
 import pylab
 from itertools import combinations
 
+
 ##########################################################
-class Agent():      
+class Agent:
+    def __init__(self, pointNumber, mapSize):
+        self.pointNumber = pointNumber
+        self.mapSize = mapSize
 
-  def __init__(self, pointNumber, mapSize):
-        self.pointNumber=pointNumber
-        self.mapSize=mapSize
+    def agents(self):  # speed and parameter
+        self.speed = [round(random.uniform(0, 3), 2)]  # speed between 0 and 3 with 2 decimal
+        self.agent = [round(random.uniform(1, 3), 1)]  # define a parameter that agent will be satified
+        return self.speed, self.agent
 
-  def agents(self):   #speed and parameter
-      self.speed=[round(random.uniform(0,3),2)] #speed between 0 and 3 with 2 decimal
-      self.agent=[round(random.uniform(1,3),1)] #define a parameter that agent will be satified
-      return self.speed, self.agent
- 
-  def maps(self):
-      self.x=[round(random.uniform(0,self.mapSize),2)]
-      self.y=[round(random.uniform(0,self.mapSize),2)]
-      return self.x, self.y
-  
-  
-  
+    def maps(self):
+        self.x = [round(random.uniform(0, self.mapSize), 2)]
+        self.y = [round(random.uniform(0, self.mapSize), 2)]
+        return self.x, self.y
+
+
 #####################################################
-pointNumber=5
-mapSize=200
+pointNumber = 5
+mapSize = 200
 
-objects=[]
-positions={}
+objects = []
+positions = {}
 
-objects=Agent(pointNumber,mapSize)
+objects = Agent(pointNumber, mapSize)
 
 for i in range(pointNumber):
-  positions[i]=objects.maps()
+    positions[i] = objects.maps()
 
-fig = plt.figure(figsize=(16,8))
+fig = plt.figure(figsize=(16, 8))
 ax1 = fig.add_subplot(111)
-ax1.set_ylim(0-10, mapSize+10) 
-ax1.set_xlim(0-10, mapSize+10) 
-colors = ['red','brown','orange','yellow','blue'] #5 attractions
-attractions=['little red','little brown','little orange','little yellow','little blue']
-plt.ion() #make code continue
-fig.show() 
-
+ax1.set_ylim(0 - 10, mapSize + 10)
+ax1.set_xlim(0 - 10, mapSize + 10)
+colors = ['red', 'brown', 'orange', 'yellow', 'blue']  # 5 attractions
+attractions = ['little red', 'little brown', 'little orange', 'little yellow', 'little blue']
+plt.ion()  # make code continue
 
 for p in range(pointNumber):
+    ax1.scatter(positions[p][0], positions[p][1], c=colors[p])
+    ax1.plot(positions[p][0], positions[p][1], c=colors[p], label=f'attractions={attractions[p]}')
 
-    ax1.scatter(positions[p][0],positions[p][1], c=colors[p])
-    ax1.plot(positions[p][0],positions[p][1], c=colors[p], label=f'attractions={attractions[p]}')
 
-ax1.plot(0,0, c='black', label=f'entry/exit') #entry/exit
-         
-ax1.scatter(0,0, c='black')
-    
+print('hej')
+#ax1.plot(0, 0, c='black', label=f'entry/exit')  # entry/exit
+
+ax1.scatter(0, 0, c='black')
 
 fig.canvas.draw()
 ax1.set_xlabel('x')
 ax1.set_ylabel('y')
 ax1.set_title('map')
-ax1.legend() 
+ax1.legend()
+fig.show()
