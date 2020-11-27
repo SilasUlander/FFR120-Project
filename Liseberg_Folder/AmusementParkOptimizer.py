6887979128c7c484d0ced9ac2737d0dd8153ba47
Silas Ulander <silasu@student.chalmers.se>
2020-11-27 11:28:04 +0100

    Map structure done

--- Liseberg_Folder/AmusementParkOptimizer.py
+++ Liseberg_Folder/AmusementParkOptimizer.py
@@ -18,11 +18,8 @@ probAgentSpawns = 1  # Probability for agent spawning at each timestep
 
 # Map parameters
 mapSize = 1000
-entrance_color = 'black'
-ground_color = '#27FF4B'
+
 parkEntrances = [[0, 700], [150, 0], [750, 0], [1000, 750], [450, 1000]]
-colors = ['red', 'brown', 'orange', 'yellow', 'blue']
-attractions = ['little red', 'little brown', 'little orange', 'little yellow', 'little blue']
 attractionCorners = [[[0, 100, 0, 600], [0, 400, 400, 600]],
                      [[200, 700, 0, 300]],
                      [[800, 1000, 0, 700]],
@@ -34,10 +31,17 @@ attractionEntrances = [[280, 350, 400, 420],
                        [500, 520, 650, 720],
                        [230, 300, 800, 820]]
 
-ParkMap = Map(entrances=attractionEntrances, mapSize=mapSize)
+ParkMap = Map(mapSize=mapSize,
+              parkEntrances=parkEntrances,
+              attractionEntrances=attractionEntrances,
+              attractionCorners=attractionCorners)
+
+colors = ['red', 'brown', 'orange', 'yellow', 'blue']
+attractions = ['little red', 'little brown', 'little orange', 'little yellow', 'little blue']
+entrance_color = 'black'
+ground_color = '#27FF4B'
 
 fig, ax = ParkMap.make_map(corners=attractionCorners,
-                           entrances=attractionEntrances,
                            parkEntrances=parkEntrances,
                            colors=colors,
                            entrance_color=entrance_color,
--- Liseberg_Folder/Map.py
+++ Liseberg_Folder/Map.py
@@ -4,33 +4,37 @@ import matplotlib
 
 
 class Map:
-    def __init__(self, entrances, mapSize):
+    def __init__(self, mapSize, parkEntrances, attractionEntrances, attractionCorners):
+
         # Map properties
         self.mapSize = mapSize
+        self.attractionEntrances = attractionEntrances
+        self.parkEntrances = parkEntrances
+        self.attractionCorners = attractionCorners
 
         # Agents location
-        self.agents = []
+        self.agentsLocation = []
 
-    def make_map(self, corners, entrances, parkEntrances, colors, entrance_color, ground_color):
+    def make_map(self, colors, entrance_color, ground_color):
         fig, ax = plt.subplots()
         map_matrix = np.zeros((1000, 1000))
-        for i, iCorners in enumerate(corners):
+        for i, iCorners in enumerate(self.attractionCorners):
             for jCorners in iCorners:
                 x1 = jCorners[0]
                 x2 = jCorners[1]
                 y1 = jCorners[2]
                 y2 = jCorners[3]
                 map_matrix[y1:y2, x1:x2] = i + 1
-            entrance_x1 = entrances[i][0]
-            entrance_x2 = entrances[i][1]
-            entrance_y1 = entrances[i][2]
-            entrance_y2 = entrances[i][3]
+            entrance_x1 = self.attractionEntrances[i][0]
+            entrance_x2 = self.attractionEntrances[i][1]
+            entrance_y1 = self.attractionEntrances[i][2]
+            entrance_y2 = self.attractionEntrances[i][3]
             map_matrix[entrance_y1:entrance_y2, entrance_x1:entrance_x2] = -1
-        for iCoords in parkEntrances:
+        for iCoords in self.parkEntrances:
             entrance_x = iCoords[0]
             entrance_y = iCoords[1]
             map_matrix[max(0, entrance_y-20):entrance_y+20, max(0, entrance_x-20):entrance_x+20] = -1
-        map_colors = matplotlib.colors.ListedColormap([entrance_color, ground_color] + colors[:len(corners)])
+        map_colors = matplotlib.colors.ListedColormap([entrance_color, ground_color] + colors[:len(self.attractionCorners)])
         ax.imshow(map_matrix, cmap=map_colors, extent=[0, 1000, 0, 1000])
         ax.axes.get_xaxis().set_visible(False)
         ax.axes.get_yaxis().set_visible(False)
